ff2ab1ca8cd460d5a759b43337fe42805d51896c
Pablo Calderon <pabcal03@ucm.es>
2024-02-14 11:58:34 +0100

    animal update yeah baby

--- src/simulator/model/animals/Animal.java
+++ src/simulator/model/animals/Animal.java
@@ -23,8 +23,7 @@ public abstract class Animal implements Entity, AnimalInfo{
 	private SelectionStrategy _mate_strategy;
 
 	protected Animal(String genetic_code, Diet diet, double sight_range,
-			 double init_speed, SelectionStrategy mate_strategy, Vector2D pos) throws IncorrectParametersException
-	{
+			 double init_speed, SelectionStrategy mate_strategy, Vector2D pos) throws IncorrectParametersException {
 		if(genetic_code == null || sight_range < 0 || init_speed < 0 || mate_strategy == null)
 		{
 			throw new IncorrectParametersException("Please enter the correct parameters"); 
@@ -46,42 +45,81 @@ public abstract class Animal implements Entity, AnimalInfo{
 		}
 	}
 	
-	protected Animal(Animal p1, Animal p2)
-	{
+	protected Animal(Animal p1, Animal p2){
 		_dest = null;
 		_mate_target = null;
 		_baby = null;
 		_region_mngr = null;
 		_desire = 0.0;
-
-		
-		
-		
+		_genetic_code = p1.get_genetic_code(); //can i do p1._genetic_code
+		_diet = p1.get_diet();
+		_mate_strategy = p2._mate_strategy;
+		_energy = ( p1.get_energy() + p2.get_energy() ) / 2; 
+		_pos = p1.get_position().plus( Vector2D.get_random_vector(-1,1).scale(60.0*(Utils._rand.nextGaussian()+1)));
+		_sight_range = Utils.get_randomized_parameter( (p1.get_sight_range() + p2.get_sight_range())/2, 0.2 );
+		_speed = Utils.get_randomized_parameter((p1.get_speed()+p2.get_speed())/2, 0.2);
 	}
-
-	public String get_genetic_code()
-	{
-		return _genetic_code;
-	}
-	
-	
 	@Override
 	public void update(double dt) {
-
+	
+	}
+	
+	public void init(AnimalMapView reg_mngr)
+	{
+		_region_mngr = reg_mngr;
+		if (_pos == null)	
+			_pos = new Vector2D(Vector2D.get_random_vector(0,_region_mngr.get_width()-1 , Vector2D.get_random_vector(0, _region_mngr.get_height()-1);
+		
+	
 	}
 	
 	
-	 public Diet get_diet()
-	 {
-		 return _diet;
-	 }
-	 
-	 public String toString()
-	 {
-		 return _genetic_code;
-	 }
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String get_genetic_code(){
+		return _genetic_code;
+	}
+	public Diet get_diet(){
+		return _diet;
+	}
+	public String toString(){
+		return _genetic_code;
+	}
+	public State get_state(){
+		return _state;
+	}
+	public Vector2D get_position(){
+		return _pos;
+	}
+	public double get_speed(){
+		return _speed;
+	}
+	public double get_sight_range(){
+		return _sight_range;
+	}
+	public double get_energy(){
+		return _energy;
+	}
+	public double get_age(){
+		return _age;
+	}
+	public Vector2D get_destination(){
+		return _dest;
+	}
+		
 	
 	
 
